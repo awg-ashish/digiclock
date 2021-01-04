93d80ed8c5e54ef8530edadda0af56d65a162b70
Ashish <ashish.blackhawk@gmail.com>
2021-01-05 01:32:08 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import styled from "styled-components";
 import Clock from "./Components/Clock";
 function App() {
-    return (
-        <div className="App">
-            <Clock />
-        </div>
-    );
+  return (
+    <Div className="App">
+      <Clock />
+    </Div>
+  );
 }
-
+const Div = styled.div`
+  background: url("https://source.unsplash.com/-djRG1vB1pw");
+  height: 100vh;
+  width: 100%;
+  background-size: cover;
+  background-position: center;
+`;
 export default App;
--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-
+import Tilt from "./Tilt";
+import "../index.css";
 const Clock = () => {
   const [time, setTime] = useState(new Date());
-  const [duration, setDuration] = useState("--");
+  const [session, setSession] = useState("--");
   const updateClock = () => {
     setTime(new Date());
   };
   const updateDuration = () => {
     if (Number(time.getHours()) > 12) {
-      setDuration("PM");
+      setSession("PM");
     } else {
-      setDuration("AM");
+      setSession("AM");
     }
   };
   useEffect(() => {
@@ -22,6 +23,13 @@ const Clock = () => {
 
     return () => clearInterval(interval);
   });
+  const options = {
+    scale: 1.05,
+    speed: 1000,
+    max: 10,
+    glare: true,
+    "max-glare": 0.1,
+  };
   const formatHours = (t) => {
     let h = Number(t.getHours());
     if (h < 10) {
@@ -46,48 +54,67 @@ const Clock = () => {
 
   return (
     <Container>
-      <Border>
-        <Time>
-          <Div> {formatHours(time)[0]}</Div>
-          <Div> {formatHours(time)[1]} </Div>
-          <Seconds> {duration} </Seconds>
-        </Time>
-        <Duration>{formatHours(time)[2]}</Duration>
-      </Border>
+      <Tilt options={options} className="blur">
+        <Border>
+          <Div>
+            <Time> {formatHours(time)[0]}</Time>
+            <Time> {formatHours(time)[1]} </Time>
+          </Div>
+          <Seconds>{formatHours(time)[2]}</Seconds>
+          <Session> {session} </Session>
+          <DateContainer>{time.toDateString()}</DateContainer>
+        </Border>
+      </Tilt>
     </Container>
   );
 };
 
 const Container = styled.div`
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  color: #282c34;
+  color: #ddd;
 `;
 
-const Div = styled.div`
+const Time = styled.div`
   font-size: 5rem;
+  font-family: "Iceland", cursive;
 `;
 
-const Seconds = styled.div`
-  font-size: 2rem;
-  margin-top: -10px;
+const Session = styled.div`
+  font-size: 1.5rem;
+  align-self: center;
+  font-family: "Bungee Outline", cursive;
 `;
 const Border = styled.div`
   display: flex;
   position: relative;
-  padding: 2rem;
-  border: 10px solid lightblue;
+  padding: 2.5rem;
+  border-top: 2px solid #333;
+  border-left: 2px solid #333;
   text-align: right;
   border-radius: 25px;
-  background-image: linear-gradient(dodgerblue, lightblue);
+  background: rgba(0, 0, 0, 0.6);
+  box-shadow: 20px 20px 50px rgba(0, 0, 0, 0.5);
+  min-width: 500px;
 `;
-const Time = styled.div``;
-const Duration = styled.div`
+const DateContainer = styled.div`
+  font-size: 2rem;
+  position: absolute;
+  left: 50%;
+  bottom: 0;
+  transform: translateX(-50%);
+  padding: 0.5rem;
+  font-family: "Iceland", cursive;
+`;
+const Seconds = styled.div`
   font-size: 10rem;
+  font-family: "Bungee Shade", cursive;
+`;
+const Div = styled.div`
+  align-self: center;
+  padding: 1rem;
 `;
 export default Clock;
